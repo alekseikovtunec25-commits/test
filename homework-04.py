9357a2afc8a81f38fd4d46579f9962b5275f0a32
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-03-04 18:17:30 +0200

    выполнение домашнего задания №3

--- homework-04.py
+++ homework-04.py
@@ -1,6 +1,13 @@
 #Task1
-var1 = input("Please enter a word:") #рпросим ввести желаемое слово
-if len(var1) < 1: #проверяем на длинну строки
-    print("please another word") #просим ввести другое слово так как слишком коротко
-elif len(var1) > 2:
-    print(var1[:2] + var1[-2:])
+#var1 = input("Please enter a word:") #рпросим ввести желаемое слово
+#if len(var1) < 1: #проверяем на длинну строки
+#    print("please another word") #просим ввести другое слово так как слишком короткое
+#elif len(var1) > 2: #снава выполняем проверку на длинну строки
+#    print(var1[:2] + var1[-2:]) #выполняем задание для первых двух символов и последних
+
+#Task2
+number_phone = input("Please enter a number:") #просим ввести номер телефона
+if len(number_phone) < 10: #проверяем количество сиволов номера телефона
+    print("please another number") #номер не подходит по формату
+number_phone.isdigit()
+
